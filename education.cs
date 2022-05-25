168645f52b946ae81afa575024301873a26b701a
Pavel Statkevich <kill.r.04@mail.ru>
2022-05-25 21:26:27 +0300

    2

--- education.cs
+++ education.cs
@@ -1,12 +1,18 @@
 using System;
-
+using System.Collections.Generic;
 namespace EducationNumOne 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            // var dict = new Dictionary<int, string>();
+
+            decimal mony =3.0m;
+            decimal speed =4.0;
+
+            char character = 'A';
+            Console.WriteLine(character);
         }
     }
 }
