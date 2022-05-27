7650e84279283b1b372d1cb7a7f0abfaf4ed1381
Pavel Statkevich <kill.r.04@mail.ru>
2022-05-27 19:16:42 +0300

    h

--- education.cs
+++ education.cs
@@ -7,23 +7,10 @@ namespace EducationNumOne
     {
         static void Main(string[] args)
         {
-         int x = 0b11;
-         int y = 0b100;
-         int k = 0b1001001;
-
-         System.Console.WriteLine(x);
-            System.Console.WriteLine(y);
-            System.Console.WriteLine(k);
+       
+         System.Console.WriteLine("Helllo World");
+           
         }
-        static void  Varibals()
-        {
-   // var dict = new Dictionary<int, string>();
-
-            decimal mony =3.0m;
-            decimal speed =4.0;
-
-            char character = 'A';
-            Console.WriteLine(character);
-        }
+        
     }
 }
