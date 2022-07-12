615a267c2e8b327cb3a59d859e2f83b43dcf9991
Pavel <kill.r.04@mail.ru>
2022-07-12 14:11:32 +0300

    ff

---
+++ MyApp/bin/Debug/net6.0/education.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EducationNumOne
+{
+    class education
+    {
+        static void Main(string[] args)
+        {
+            string openpl = "johnsilver qwerty";
+            string pl;
+            int attempts = 1;
+        Console.WriteLine("please enter your Login Password");
+         pl = System.Console.ReadLine();
+        while(attempts <=3)
+        {
+            Console.WriteLine("Enter login pass");
+            Console.ReadLine();
+        }
+        }
+    }
+}
